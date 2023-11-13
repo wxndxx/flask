c6ff09b7f243f72711b84baaf295e7f2846de114
wxndxx <47880071+wxndxx@users.noreply.github.com>
2023-11-13 16:27:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask!\n'
+
+if __name__ == '__main__':
+    print('wow, nechego sebe vot eto da')
+    
