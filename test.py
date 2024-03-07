c1b8c82f407e60a0dbd7b3439a13dbce09af4559
wxndxx <47880071+wxndxx@users.noreply.github.com>
2024-03-07 14:27:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import sys
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    while True:
+        time.sleep(1)
+        print('Error message', file=sys.stderr)
     return 'wow, nechego sebe vot eto da\n'
 
 if __name__ == '__main__':
