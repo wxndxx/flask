146d3ff42edc80e791ad64d4a12839baf6dd0f5c
wxndxx <47880071+wxndxx@users.noreply.github.com>
2024-03-18 14:22:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!\n'
-
-if __name__ == '__main__':
-    print('wow, nechego sebe vot eto da')
-    
+    return 'Timeweb Cloud + Flask = ❤️'
